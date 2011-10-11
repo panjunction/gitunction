e8ab45abea856f07f0bbe244f24305b3f68723b7
Paul Panarese <paul@panjunction.com>
2011-10-11 14:41:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitunction::Application.config.secret_token = '4dd0ca462d7d01981965a2073954a22b3404df20f32d2e830cd31ff870722037c915fdf14a0c0d660a2dfc122becdd61f0a1dbff37be1e47eec5b40eb446a1f5'
